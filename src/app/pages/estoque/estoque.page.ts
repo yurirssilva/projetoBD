6070364673c06e930fadcab6d34569ee261edf6c
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-10-19 09:30:58 -0300

    [Estoque] Abrir modais

--- src/app/pages/estoque/estoque.page.ts
+++ src/app/pages/estoque/estoque.page.ts
@@ -1,15 +1,22 @@
+import { CadastrarProdutoPage } from './cadastrar-produto/cadastrar-produto.page';
+import { ModalController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-estoque',
-  templateUrl: './estoque.page.html',
-  styleUrls: ['./estoque.page.scss'],
+    selector: 'app-estoque',
+    templateUrl: './estoque.page.html',
+    styleUrls: ['./estoque.page.scss'],
 })
 export class EstoquePage implements OnInit {
 
-  constructor() { }
+    constructor(public modalCtrl: ModalController) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    async addProduto() {
+        let modal = await this.modalCtrl.create({ component: CadastrarProdutoPage, cssClass: 'modal-pequeno' });
+        modal.present();
+    }
 
 }
