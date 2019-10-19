c4456d73c5289ee255ae73e7cf733b847c3f9180
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-10-19 09:31:10 -0300

    [Funcionarios] Abrir Modais

--- src/app/pages/funcionarios/funcionarios.page.ts
+++ src/app/pages/funcionarios/funcionarios.page.ts
@@ -1,15 +1,29 @@
+import { CadastrarSetorPage } from './cadastrar-setor/cadastrar-setor.page';
+import { CadastrarFuncionarioPage } from './cadastrar-funcionario/cadastrar-funcionario.page';
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-funcionarios',
-  templateUrl: './funcionarios.page.html',
-  styleUrls: ['./funcionarios.page.scss'],
+    selector: 'app-funcionarios',
+    templateUrl: './funcionarios.page.html',
+    styleUrls: ['./funcionarios.page.scss'],
 })
 export class FuncionariosPage implements OnInit {
 
-  constructor() { }
+    constructor(
+        public modalCtrl: ModalController
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    async addFuncionario() {
+        let modal = await this.modalCtrl.create({ component: CadastrarFuncionarioPage, cssClass: 'modal-pequeno' })
+        modal.present();
+    }
+
+    async addSetor() {
+        let modal = await this.modalCtrl.create({ component: CadastrarSetorPage, cssClass: 'modal-pequeno' })
+        modal.present();
+    }
 }
